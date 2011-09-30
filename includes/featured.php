2cfd74ca2537fc8905b72280490b8d567ebab006
Antoine Perdaens <a.perdaens@whatever-company.com>
2011-09-30 16:56:53 +0200

    Header : Remove slide navigation + support 4 different posts
    Signed-off-by: Antoine Perdaens <a.perdaens@whatever-company.com>

--- includes/featured.php
+++ includes/featured.php
@@ -9,7 +9,7 @@ $count = 0;
     <?php
     	$shownposts = array(); // An array for storing the posts we don't want to display on the homepage (they're in the featured slider already).
 		global $shownposts;
-		$featposts = 3; // Number of featured entries to be shown
+		$featposts = 4; // Number of featured entries to be shown
 		$feat_tags_array = explode(',',get_option('woo_featured_tags')); // Tags to be shown
         foreach ($feat_tags_array as $tags){
 			$tag = get_term_by( 'name', trim($tags), 'post_tag', 'ARRAY_A' );
@@ -26,12 +26,12 @@ $count = 0;
 		<?php while (have_posts()) : the_post(); ?>
 		<?php if (!woo_image('return=true')) continue; // Skip post if it doesn't have an image ?>
         <?php $shownposts[] = $post->ID; $count++; ?>
-
+		<?php if ($count <= 2) { ?>
 		<div class="slide">
 
 			<?php woo_image("key=image&width=652&height=290"); ?>
 
-			<div class="caption" <?php if ($count == 1) { ?>style="bottom:0"<?php } ?>>
+			<div class="caption" style="bottom:0;">
 				<span class="post-meta featured-category"><?php the_category(', '); ?></span>
 				<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 				<div class="entry">
@@ -40,22 +40,23 @@ $count = 0;
 			</div><!-- /.caption -->
 
 		</div><!-- /.slide -->
-
+		<?php } ?>
 		<?php endwhile; ?>
 
 	</div><!-- /#slides_container -->
 
 	<?php endif; ?>
 
-	<div id="slide-nav" class="pagination col-right">
+	<div id="slide-nav" class="col-right">
 
 	<?php rewind_posts(); ?>
 	<?php if (have_posts()) : $count = 0; ?>
 		<ul>
-			<?php while (have_posts()) : the_post(); ?>
+			<?php the_post(); // Skip the first post?>
+			<?php while (have_posts()) : the_post(); $count++; ?>
 			<?php if (!woo_image('return=true')) continue; // Skip post if it doesn't have an image ?>
 				<li>
-					<a href="#">
+					<a href="<?php the_permalink() ?>">
 						<?php woo_image("key=image&width=307&height=96&link=img"); ?>
 
 						<?php $category = get_the_category(); ?>
